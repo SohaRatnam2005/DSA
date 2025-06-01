4a73bef6c70c6f11ef9ffe4dbec43bb33677d8ee
Soha <soharatnam12095@gmail.com>
2025-06-01 23:10:47 +0530

    Time: 2 ms (82.14%) | Memory: 46 MB (65.40%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,15 +1,17 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int nonzero = 0; // Pointer to place the next non-zero element
+       int pos = 0 ;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                // Swap non-zero element with the element at `nonzero`
-                int temp = nums[i];
-                nums[i] = nums[nonzero];
-                nums[nonzero] = temp;
-                nonzero++; // Increment the nonzero pointer
+        for ( int i =0 ; i < nums.length ; i++ ){
+            if( nums[i] != 0){
+                nums[pos] = nums[i];
+                pos++;
             }
         }
+            while (pos < nums.length) {
+            nums[pos++] = 0;
+        }
+        
     }
-}
+        }
+    
