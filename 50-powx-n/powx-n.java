9ced864a5953a43a774db027a7e743a266af30dd
Soha <soharatnam12095@gmail.com>
2026-04-12 00:36:15 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (38.76%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,18 @@
+class Solution {
+    public double myPow(double x, int n) {
+       double res = 1.0 ;
+       long N = n ;
+       if( N < 0 ){
+        x = 1/x ;
+        N = -N ;
+       }
+        while( N != 0 ){
+            if((N & 1 ) != 0){
+                res *= x ;
+            }
+            x *= x ;
+            N >>>= 1 ;
+        }
+       return res ;
+    }
+}
