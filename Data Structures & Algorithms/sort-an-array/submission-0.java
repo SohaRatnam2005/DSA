e181b3e3ed84652a825134be6f4afdb91d0eb830
Soha <soharatnam12095@gmail.com>
2026-06-25 05:55:30 +0530

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.java
@@ -0,0 +1,38 @@
+class Solution {
+    public void Heapify(int[] arr , int n , int i ){
+        int largest = i ;
+        int left = 2 * i + 1 ;
+        int right = 2*i +2 ;
+        
+        if(left < n && arr[left]>arr[largest])
+            largest = left ;
+
+        if( right < n && arr[right]> arr[largest])
+            largest = right ;
+
+        if( largest != i){
+            int temp = arr[i];
+            arr[i]=arr[largest];
+            arr[largest]= temp;
+            Heapify(arr,n,largest);
+        }
+
+       
+        }
+    
+    public int[] sortArray(int[] nums) {
+        int n = nums.length  ;
+        for(int i = n/2 - 1 ; i>=0 ; i--){
+            Heapify(nums,n,i);
+        }
+        for(int i = n -1 ; i>0 ; i--){
+            int temp = nums[0];
+            nums[0] = nums[i];
+            nums[i]  = temp ;
+
+            Heapify(nums , i , 0);
+        }
+        return nums;
+        
+    }
+}
