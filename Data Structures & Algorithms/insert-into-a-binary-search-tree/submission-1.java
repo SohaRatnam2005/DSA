357b0149cbaab8c05d0453555e2aa3ea8ae9e632
Soha <soharatnam12095@gmail.com>
2026-09-05 05:29:36 +0530

    Add: insert-into-a-binary-search-tree - submission-1

---
+++ Data Structures & Algorithms/insert-into-a-binary-search-tree/submission-1.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+       if(root == null) return new TreeNode(val);
+
+       TreeNode node = root ;
+       while(true){
+        if(node.val > val){
+            if(node.left == null){
+                node.left = new TreeNode(val);
+                break ;
+            }
+            node = node.left ;
+        }
+        else{
+            if(node.right == null){
+                node.right = new TreeNode(val);
+                break ;
+            }
+            node = node.right;
+        }
+       }
+       return root;
+    }
+}
